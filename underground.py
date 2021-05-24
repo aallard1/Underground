d6c0c41f9145bcecf0504308c39ed4d3bccb8812
Alexandra <aallard170@gmail.com>
2021-05-24 11:26:19 -0700

    Implemented fleeing combat

--- underground.py
+++ underground.py
@@ -30,6 +30,7 @@ def intro(inventory):
     print("The hooded figure melts away into the darkness.")
     print("What's this? They dropped a lanturn...")
     inventory.append('lanturn')
+    print("Well, I guess I'm on my own now... ")
     user_input = input('')
     while user_input == '':
         break
@@ -39,7 +40,7 @@ def get_stats(stats):
     stats['defence_stat'] = random.randint(MIN_START_STAT, MAX_START_STAT)
 
 def menu(stats, inventory, enemy):
-    print("Well, I guess I'm on my own now... what should I do?")
+    print("What should I do?")
     print("Move: 1")
     print("Check Stats: 2")
     print("Check Inventory: 3")
@@ -50,7 +51,7 @@ def menu(stats, inventory, enemy):
             print("You move forward in the darkness...")
             enemy_encounter_chance = random.random()
             # item_chance = random.random()
-            enemy_encounter(stats, enemy, enemy_encounter_chance)
+            enemy_encounter(stats, enemy, enemy_encounter_chance, inventory)
             user_action = str(input("Choose an action: "))
         elif user_action == '2':
             check_stats(stats)
@@ -69,18 +70,43 @@ def check_stats(stats):
     print(f"Defence: {stats['defence_stat']}")
     print(f"Health: {stats['health_stat']}")
 
-def enemy_encounter(stats, enemy, enemy_encounter_chance):
+def enemy_encounter(stats, enemy, enemy_encounter_chance, inventory):
     if enemy_encounter_chance >= 0.5:
-        combat(stats, enemy)
+        combat(stats, enemy, inventory)
     else:
         print("Nothing interesting happens...")
 
-def combat(stats, enemy):
+def flee(flee_chance, stats, inventory, enemy):
+    if flee_chance >= 0.5:
+        print("You were able to flee combat...")
+        menu(stats, inventory, enemy)
+    else:
+        print("You were not able to flee combat.")
+
+def combat(stats, enemy, inventory):
     print("An enemy approaches you.")
     print("Attack: 1")
     print("Defend: 2")
-    print("Flee: 3")
+    print("Use Item: 3")
+    print("Flee: 4")
     user_action = str(input("Choose an action: "))
+    while user_action != '':
+        if user_action == '1':
+            print("You attack the enemy...")
+            user_action = str(input("Choose an action: "))
+        elif user_action == '2':
+            print("You defend yourself...")
+            user_action = str(input("Choose an action: "))
+        elif user_action == '3':
+            print(f"Inventory: {inventory}")
+        elif user_action == '4':
+            print("You attempt to flee...")
+            flee_chance = random.random()
+            flee(flee_chance, stats, inventory, enemy)
+            user_action = str(input("Choose an action: "))
+        else:
+            print("I don't think I can do that...")
+            user_action = str(input("Choose an action: "))
 
 if __name__ == "__main__":
     main()
