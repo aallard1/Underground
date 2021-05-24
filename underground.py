6f3911099e09844d280210e7fe0c414561af84c4
Alexandra <aallard170@gmail.com>
2021-05-24 11:06:28 -0700

    Started implementing combat

--- underground.py
+++ underground.py
@@ -11,6 +11,7 @@ MIN_START_STAT = 1
 def main():
     stats = {'attack_stat': 0, 'defence_stat': 0, 'health_stat': 20}
     inventory = []
+    items = []
     enemy = {'attack_stat': 1, 'defence_stat': 1, 'health_stat': 5}
     intro(inventory)
     get_stats(stats)
@@ -42,11 +43,13 @@ def menu(stats, inventory, enemy):
     print("Move: 1")
     print("Check Stats: 2")
     print("Check Inventory: 3")
+    print("Quit: 4")
     user_action = str(input("Choose an action: "))
     while user_action != '':
         if user_action == '1':
             print("You move forward in the darkness...")
             enemy_encounter_chance = random.random()
+            # item_chance = random.random()
             enemy_encounter(stats, enemy, enemy_encounter_chance)
             user_action = str(input("Choose an action: "))
         elif user_action == '2':
@@ -55,6 +58,8 @@ def menu(stats, inventory, enemy):
         elif user_action == '3':
             print(f"Inventory: {inventory}")
             user_action = str(input("Choose an action: "))
+        elif user_action == '4':
+            quit()
         else:
             print("I don't think I can do that...")
             user_action = str(input("Choose an action: "))
@@ -72,6 +77,10 @@ def enemy_encounter(stats, enemy, enemy_encounter_chance):
 
 def combat(stats, enemy):
     print("An enemy approaches you.")
+    print("Attack: 1")
+    print("Defend: 2")
+    print("Flee: 3")
+    user_action = str(input("Choose an action: "))
 
 if __name__ == "__main__":
     main()
